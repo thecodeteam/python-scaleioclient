5f47d70564a323a1bd14fe7dbb0f5cf48af195ab
Andrey Pavlov <andrey.mp@gmail.com>
2016-10-13 16:43:00 +0300

    Bump version and update requirements for newton

--- setup.py
+++ setup.py
@@ -45,9 +45,9 @@ setup(
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",],
     install_requires=[
-        'oslo.config>=3.7.0',
-        'requests>=2.8.1,!=2.9.0',
-        'urllib3>=1.8.3',
+        'oslo.config>=3.14.0',
+        'requests>=2.10.0',
+        'urllib3>=1.15.1',
         'enum34',
     ],
     scripts=glob("tests/functional/*"),
